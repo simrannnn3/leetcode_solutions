2607f3712fa928799e5901ad6aeac410569eeee4
Simran Singhania <147339765+simrannnn3@users.noreply.github.com>
2026-09-02 19:52:26 +0530

    Time: 402 ms (49.32%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,15 @@
+SELECT
+    query_name,
+
+    ROUND(
+        SUM(rating / position) / COUNT(*),
+        2
+    ) AS quality,
+
+    ROUND(
+        SUM(rating < 3) * 100.0 / COUNT(*),
+        2
+    ) AS poor_query_percentage
+
+FROM Queries
+GROUP BY query_name;
